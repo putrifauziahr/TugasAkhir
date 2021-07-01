7ede331a25a98415ff69624fcf2442097e47fa5b
PutriFauziahRahman <Iin>
2021-07-01 13:07:04 +0700

    Kuisioner part 8

--- resources/views/petani/layouts/petani2.blade.php
+++ resources/views/petani/layouts/petani2.blade.php
@@ -120,10 +120,10 @@
           <div class="col-md-4 col-sm-6">
             <div class="footer-info">
               <div class="section-title">
-                <h2>Headquarter</h2>
+                <h2>Alamat</h2>
               </div>
               <address>
-                <p>1800 dapibus a tortor pretium,<br> Integer nisl dui, ABC 12000</p>
+                <p>Desa Babakan Jaya, Kecamatan Gabuswetan</p>
               </address>
 
               <ul class="social-icon">
@@ -131,49 +131,31 @@
                 <li><a href="#" class="fa fa-twitter"></a></li>
                 <li><a href="#" class="fa fa-instagram"></a></li>
               </ul>
-
-              <div class="copyright-text">
-                <p>Copyright &copy; 2018 Company</p>
-                <p>Design: <a rel="nofollow" href="http://templatemo.com" title="html5 templates" target="_parent">Template Mo</a></p>
-                <p>Distribution: <a href="https://themewagon.com/">ThemeWagon</a></p>
-              </div>
             </div>
           </div>
 
           <div class="col-md-4 col-sm-6">
             <div class="footer-info">
               <div class="section-title">
-                <h2>Contact Info</h2>
+                <h2>Kontak Info</h2>
               </div>
               <address>
-                <p>+65 2244 1100, +66 1800 1100</p>
-                <p><a href="mailto:youremail.com">hello@youremail.co</a></p>
+                <p>085321041904</p>
+                <p><a href="mailto:youremail.com">bpp.gabuswetan@gmail.com</a></p>
               </address>
-
-              <div class="footer_menu">
-                <h2>Quick Links</h2>
-                <ul>
-                  <li><a href="#">Career</a></li>
-                  <li><a href="#">Investor</a></li>
-                  <li><a href="#">Terms & Conditions</a></li>
-                  <li><a href="#">Refund Policy</a></li>
-                </ul>
-              </div>
             </div>
           </div>
 
           <div class="col-md-4 col-sm-12">
             <div class="footer-info newsletter-form">
               <div class="section-title">
-                <h2>Newsletter Signup</h2>
+                <h2>Hak Cipta</h2>
               </div>
               <div>
-                <div class="form-group">
-                  <form action="#" method="get">
-                    <input type="email" class="form-control" placeholder="Enter your email" name="email" id="email" required="">
-                    <input type="submit" class="form-control" name="submit" id="form-submit" value="Send me">
-                  </form>
-                  <span><sup>*</sup> Please note - we do not spam your email.</span>
+                <div class="copyright-text">
+                  <p>Copyright &copy; 2018 Company</p>
+                  <p>Design: <a rel="nofollow" href="http://templatemo.com" title="html5 templates" target="_parent">Template Mo</a></p>
+                  <p>Distribution: <a href="https://themewagon.com/">ThemeWagon</a></p>
                 </div>
               </div>
             </div>
